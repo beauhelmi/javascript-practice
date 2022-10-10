f228d9314b99d0aa769166086594a8180b28017d
b34u <beauhelmi@gmail.com>
2022-10-10 23:19:20 +0800

    last commit of the day

--- if-else-if.js
+++ if-else-if.js
@@ -14,10 +14,10 @@
 //     console.log("Not elegible, you have already gotten one")
 // }
 
-console.log(4 === 3)  // false
-console.log(5 > 2)    // true
-console.log(12 > 12)  // false
-console.log(3 < 0)    // false
-console.log(3 >= 3)   //  true
-console.log(11 <= 11) // true
-console.log(3 <= 2)   // false
+// console.log(4 === 3)  // false
+// console.log(5 > 2)    // true
+// console.log(12 > 12)  // false
+// console.log(3 < 0)    // false
+// console.log(3 >= 3)   //  true
+// console.log(11 <= 11) // true
+// console.log(3 <= 2)   // false
--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-let firstCard = Math.floor((Math.random() * 11) + 1);
-let secondCard = Math.floor((Math.random() * 11) + 1);
-let sum;
+// let firstCard = Math.floor((Math.random() * 11) + 1);
+// let secondCard = Math.floor((Math.random() * 11) + 1);
+// let sum;
 
-sum = firstCard + secondCard;
+// sum = firstCard + secondCard;
 
-const newLocal = false;
-let hasBlackJack = newLocal;
-let newLocal_1 = true;
-let isAlive = newLocal_1;
-console.log(sum)
+// const newLocal = false;
+// let hasBlackJack = newLocal;
+// let newLocal_1 = true;
+// let isAlive = newLocal_1;
+// console.log(sum)
 
-if  (sum <= 20)  {
-    console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳" )
-    hasBlackJack = true;
-} else  {
-    console.log("You are out of the game! 😭")
-    isAlive = false;
-}
+// if  (sum <= 20)  {
+//     console.log("Do you want to draw a new card? 🙂")
+// } else if (sum === 21) {
+//     console.log("Wohoo! You've got Blackjack! 🥳" )
+//     hasBlackJack = true;
+// } else  {
+//     console.log("You are out of the game! 😭")
+//     isAlive = false;
+// }
 
-console.log(isAlive)
+// console.log(isAlive)
